10ce5ca8cf483127aeb0370882093cca6163863f
stephen <spmallette@gmail.com>
2019-11-13 21:29:58 -0500

    Another timeout bump to solve travis.

--- gremlin-server/src/test/java/org/apache/tinkerpop/gremlin/server/GremlinServerIntegrateTest.java
+++ gremlin-server/src/test/java/org/apache/tinkerpop/gremlin/server/GremlinServerIntegrateTest.java
@@ -1196,7 +1196,7 @@ public class GremlinServerIntegrateTest extends AbstractGremlinServerIntegration
 
     @Test
     public void shouldFailOnDeadHost() throws Exception {
-        final Cluster cluster = TestClientFactory.build().create();
+        final Cluster cluster = TestClientFactory.build().maxWaitForConnection(90000).create();
         final Client client = cluster.connect();
 
         // ensure that connection to server is good
